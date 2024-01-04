c3ffd1dc363fef1bf74bb3bc55a3f4390d66059f
jsy10 <74418510+jsy10@users.noreply.github.com>
2024-01-04 14:41:39 +0200

    Exam changes

--- src/components/Exam/Exam.js
+++ src/components/Exam/Exam.js
@@ -2,74 +2,294 @@
 import { React, useEffect, useState } from 'react';
 import './Exam.css';
 import { Link, useParams } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group'
+// import Button from '@mui/material/Button';
 
 const Exam = () => {
     const [start, setStart] = useState(false)
+    const [examEnded, setExamEnded] = useState(false)
+    const [nextQuestion, setNextQuestion] = useState(false)
+    const [mark, setMark] = useState()
     const [answers, setAnswers] = useState([])
     const [answer, setAnswer] = useState("")
+    const [questionNumber, setQuestionNumber] = useState(0)
     const { id } = useParams()
 
-    const questions = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",]
+    const Exam = [
+        {
+            examName: "Exam Name",
+            examDescriprion: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            Time: 60
+        }]
 
-    const correctAnswers = ["A", "A", "A", "A", "A", "A", "A", "A", "A", "A"]
-
-    const [questionNumber, setQuestionNumber] = useState(0)
-
-    // useEffect(
-    //     s
-    //     ,questionNumber)
+    const Questions = [
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        },
+        {
+            question: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            A: "A",
+            B: "B",
+            C: "C",
+            D: "D",
+            correctAnswer: "A"
+        }]
 
     const handleNext = () => {
-        if (answer != "") {
+        if (answer !== "") {
             //Update Answers and move to next question
             setAnswers([...answers, answer])
             setQuestionNumber(questionNumber + 1)
 
             //Reset the answer value
             setAnswer("")
+
+            // Move to next question
+            setNextQuestion(true)
         }
     }
 
     //need to fix the unupdated array on last submit
     const handleSubmit = () => {
+        // console.log("answers:[" + answers + "]=" + answers.length)
         // console.log("handlesubmit")
+        // answer !== "" && setQuestionNumber(questionNumber + 1)
+        // console.log("answers:[" + answers + "]=" + answers.length)
+
+        // console.log("Questions[Correctanswer]: " + Questions[correctAnswer])
+
+        // let count = 0;
+        // for (let i = 0; i <= Questions.length - 1; i++) {
+        //     // console.log("i: " + i)
+
+        //     if (Questions[i]["correctAnswer"] === answers[i]) {
+        //         // console.log(answers[i])
+
+        //         count++
+        //     }
+        // }
         handleNext()
-        // console.log("answers: " + answers)
-        // console.log("Correctanswers: " + correctAnswers)
+        // Exam has ended
+        setStart(false)
+        setExamEnded(true)
 
-        let count = 0;
-        for (let i = 0; i <= correctAnswers.length - 1; i++) { 
-            // console.log("i: "+ i)
-            if(correctAnswers[i] === answers[i]) 
-            {
-                // console.log(answers[i])
-
-                count++
-            }
-        }
-
-        // console.log(count)
-        console.log("your score is "+count + " out of " + correctAnswers.length)
+        // // Set Mark 
+        // console.log("your score is " + count + " out of " + Questions.length)
     }
 
 
+
+    // const evaluateMark = () => {
+    //     console.log("evaluate mark has been called")
+    //     let count = 0;
+    //     for (let i = 0; i < Questions.length; i++) {
+    //         // console.log("i: " + i)
+
+    //         if (Questions[i]["correctAnswer"] === answers[i]) {
+    //             // console.log(answers[i])
+
+    //             count++
+    //         }
+    //     }
+    //     console.log("your score is " + count + " out of " + Questions.length)
+    //     console.log("Answers: " + answers + " , length of " + answers.length)
+    //     setMark(count)
+    // }
+
+    // Evaluate Mark
+    useEffect(() => {
+
+        let count = 0;
+
+        // For each question check if the answeer is correct
+        for (let i = 0; i < Questions.length; i++) {
+            if (Questions[i]["correctAnswer"] === answers[i]) {
+                count++
+            }
+        }
+        // Set Mark
+        setMark(count)
+    }
+
+        , [examEnded])
+
     return (
         <>
-            <h1>Afou xalia tha grapseis</h1>
-            <h1>re</h1>
+            {/* <CSSTransition
+            in={start}
+            // nodeRef={nodeRef}
+            timeout={800}
+            transitionName = "fade"
+            classNames="container result"
+            unmountOnExit
+            onEnter={() => setShowButton(false)}
+            onExited={() => setNextQuestion(false)} 
+
+
+            // className="container result"
+            // component="div"
+            // transitionName="fade"
+            // transitionEnterTimeout={800}
+            // transitionLeaveTimeout={500}
+            // transitionAppear
+            // transitionAppearTimeout={500}
+            >*/}
+            {/* </CSSTransition> */}
+            {/* TODO: use csstransition on close setStart(false)
+         <CSSTransition
+            in={i === questionNumber}
+            nodeRef=null
+            timeout={800}
+            transitionName = "fade"
+            classNames="container result"
+            unmountOnExit
+            onEnter={}
+            onExited={} */}
             {start ?
-                <div>
-                    <h1>Question {questionNumber + 1}:</h1>
-                    <p>{questions[questionNumber]}</p>
+                <div className="container">
+                    <CSSTransition
+                        in={0 === questionNumber}
+                        nodeRef={null}
+                        timeout={800}
+                        transitionName="fade"
+                        classNames="container result"
+                        unmountOnExit
+                    // onEnter={}
+                    // onExited={}
+                    >
+                        <div className="container">
+                            <div className="questionCount">Question {questionNumber + 1} of {Questions.length}:</div>
+                            <h2 className="question">{Questions[questionNumber]["question"]}</h2>
+                            <div className="answerOptions">
+                                <ul className="answerOptions">
+                                    <li className="answerOption">
+                                        <label className="radioCustomLabel">
+                                            <input type="radio"
+                                                className="radioCustomButton"
+                                                name="myRadio"
+                                                value="A"
+                                                checked={answer === "A"}
+                                                onChange={(e) => setAnswer(e.target.value)} />
+                                            {Questions[questionNumber]["A"]}
+                                        </label>
+                                    </li>
+                                    <li className="answerOption">
+                                        <label className="radioCustomLabel">
+                                            <input type="radio"
+                                                className="radioCustomButton"
+                                                name="myRadio"
+                                                value="B"
+                                                checked={answer === "B"}
+                                                onChange={(e) => setAnswer(e.target.value)} />
+                                            {Questions[questionNumber]["B"]}
+                                        </label>
+                                    </li>
+                                    <li className="answerOption">
+                                        <label className="radioCustomLabel">
+                                            <input type="radio"
+                                                className="radioCustomButton"
+                                                name="myRadio"
+                                                value="C"
+                                                checked={answer === "C"}
+                                                onChange={(e) => setAnswer(e.target.value)} />
+                                            {Questions[questionNumber]["C"]}
+                                        </label>
+                                    </li>
+                                    <li className="answerOption">
+                                        <label className="radioCustomLabel">
+                                            <input type="radio"
+                                                className="radioCustomButton"
+                                                name="myRadio"
+                                                value="D"
+                                                checked={answer === "D"}
+                                                onChange={(e) => setAnswer(e.target.value)} />
+                                            {Questions[questionNumber]["D"]}
+                                        </label>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            {Questions.length - 1 === questionNumber
+                                // ? <Button variant="contained" disableElevation onClick={handleSubmit}>End Exam</Button>
+                                // : <Button variant="contained" disableElevation onClick={handleNext}>Next Question</Button>}
+                                ? <button onClick={handleSubmit}>End Exam</button>
+                                : <button onClick={handleNext}>Next Question</button>} 
+                        </div>
+                    </CSSTransition>
+
+
+
+                    {/* <h1>Question {questionNumber + 1} of {Questions.length}:</h1>
+                    <p>{Questions[questionNumber]["question"]}</p>
                     <div>
                         <label>
                             <input type="radio"
@@ -77,7 +297,7 @@ const Exam = () => {
                                 value="A"
                                 checked={answer === "A"}
                                 onChange={(e) => setAnswer(e.target.value)} />
-                            Option 1
+                            {Questions[questionNumber]["A"]}
                         </label>
 
                         <label>
@@ -86,7 +306,7 @@ const Exam = () => {
                                 value="B"
                                 checked={answer === "B"}
                                 onChange={(e) => setAnswer(e.target.value)} />
-                            Option 2
+                            {Questions[questionNumber]["B"]}
                         </label>
 
                         <label>
@@ -95,7 +315,7 @@ const Exam = () => {
                                 value="C"
                                 checked={answer === "C"}
                                 onChange={(e) => setAnswer(e.target.value)} />
-                            Option 3
+                            {Questions[questionNumber]["C"]}
                         </label>
 
                         <label>
@@ -104,23 +324,40 @@ const Exam = () => {
                                 value="D"
                                 checked={answer === "D"}
                                 onChange={(e) => setAnswer(e.target.value)} />
-                            Option 4
+                            {Questions[questionNumber]["D"]}
                         </label>
                     </div>
                     <h3>Attention: Cannot go back to previous questions!!</h3>
 
-                    {correctAnswers.length - 1 === questionNumber
+                    {Questions.length - 1 === questionNumber
                         ? <button onClick={handleSubmit}>End Exam</button>
-                        : <button onClick={handleNext}>Next Question</button>}
+                        : <button onClick={handleNext}>Next Question</button>} */}
 
-                    {/* <h4>answer: {answer}</h4> */}
+                    {/* // answer !== "" && setQuestionNumber(questionNumber + 1)
+                            // console.log("answers:[" + answers + "]=" + answers.length) */}
+
                 </div>
+
+                // Page before and after starting the exam 
                 : <div>
-                    <h1>Exam: Exam name</h1>
-                    <p>For this exam you need to answer 10 questions in 60 minutes!</p>
-                    <button onClick={() => setStart(!start)}>Let's Start</button>
+                    <h1>Exam: {Exam[0]["examName"]}</h1>
+                    <p>Exam: {Exam[0]["examDescriprion"]}</p>
+
+                    {/*  Page after exam has ended */}
+                    {examEnded
+                        ? <div>
+                            <div>Exam Completed</div>
+                            <div>Your Mark is {mark} / 10</div>
+                        </div>
+                        // Page before exam starts
+                        : <div>
+                            <p>For this exam you need to answer {Questions.length} questions in {Exam[0]["Time"]} minutes!</p>
+                            <button onClick={() => setStart(!start)}>Let's Start</button>
+                        </div>}
                 </div>
+
             }
+
         </>
     );
 };
