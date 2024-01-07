0dd29971ced3905d3b60cd656ba999e38dcd90af
wiqrug <papavasiliou.alexios@gmail.com>
2024-01-07 15:18:33 +0200

    minor changes in style and added button in MyCertificates in order to display them prettier

--- src/components/CandidateCertificates/CandidateCertificates.jsx
+++ src/components/CandidateCertificates/CandidateCertificates.jsx
@@ -1,8 +1,8 @@
-import { useObtainedCertificates } from "hooks/useObtainedCertificates";
-import React from "react";
-import { useUnobtainedCertificates } from "hooks/useUnobtainedCertificates";
+import React, { useState } from "react";
 import "./candidate-certificate.css";
-import { useAvailableCertificates } from "hooks/useAvailableCertificates";
+import ObtainedCertificates from "./ObtainedCertificates";
+import UnobtainedCertificates from "./UnobtainedCertificates";
+import AvailableCertificates from "./AvailableCertificates";
 
 // O 1003 exei to certificate obtained
 
@@ -18,81 +18,32 @@ import { useAvailableCertificates } from "hooks/useAvailableCertificates";
 //So user can choose if he wants to see the Obtained, Unobtained, Remaining certificates
 //By default the Obtained should be loaded
 //Need to fix css caps and lower cases
-
-function ObtainedCertificates() {
-  const obtainedCertificates = useObtainedCertificates();
-  return (
-    <div>
-      <div className="Certificate-Details-Title">Obtained Certificates</div>
-      {obtainedCertificates?.map(({ title, description }) => (
-        <article key={title}>
-          <img
-            src="https://raw.githubusercontent.com/wiqrug/wiqrug.github.io/main/images/DALL%C2%B7E%202023-10-26%2018.43.43%20-%20Wide%20cartoon%20artwork%20with%20a%20gentle%20cream-colored%20backdrop.%20Playful%20anime%20clouds%20float%20around%2C%20some%20with%20cute%20expressions%2C%20ensuring%20the%20middle%20remains%20.png"
-            alt="img-logo"
-          />
-
-          <h4>{title}</h4>
-          <h4>{description}</h4>
-          <p>Successfully Completed :)</p>
-          {/* Render other details as needed */}
-        </article>
-      ))}
-    </div>
-  );
-}
-
-function UnobtainedCertificates() {
-  const unobtainedCertificates = useUnobtainedCertificates();
-  return (
-    <div>
-      <div className="Certificate-Details-Title">Unobtained Certificates</div>
-      {unobtainedCertificates
-        // @ts-ignore
-        ?.map(({ title, description }) => (
-          <article key={title}>
-            <img
-              src="https://raw.githubusercontent.com/wiqrug/wiqrug.github.io/main/images/DALL%C2%B7E%202023-10-26%2018.43.43%20-%20Wide%20cartoon%20artwork%20with%20a%20gentle%20cream-colored%20backdrop.%20Playful%20anime%20clouds%20float%20around%2C%20some%20with%20cute%20expressions%2C%20ensuring%20the%20middle%20remains%20.png"
-              alt="img-logo"
-            />
-
-            <h4>{title}</h4>
-            <h4>{description}</h4>
-            <p>Click here to take the exam test</p>
-          </article>
-        ))}
-    </div>
-  );
-}
-
-function AvailableCertificates() {
-  const availableCertificates = useAvailableCertificates();
-  return (
-    <div>
-      <div className="Certificate-Details-Title">Available Certificates</div>
-      {availableCertificates
-        // @ts-ignore
-        ?.map(({ title, description }) => (
-          <article key={title}>
-            <img
-              src="https://raw.githubusercontent.com/wiqrug/wiqrug.github.io/main/images/DALL%C2%B7E%202023-10-26%2018.43.43%20-%20Wide%20cartoon%20artwork%20with%20a%20gentle%20cream-colored%20backdrop.%20Playful%20anime%20clouds%20float%20around%2C%20some%20with%20cute%20expressions%2C%20ensuring%20the%20middle%20remains%20.png"
-              alt="img-logo"
-            />
-
-            <h4>{title}</h4>
-            <h4>{description}</h4>
-            <p>Buy this certificate</p>
-          </article>
-        ))}
-    </div>
-  );
-}
+//Also these components could be one component who dynamically changes
 
 const CandidateCertificates = () => {
+  const [selectedOption, setSelectedOption] = useState("obtained");
+
+  const handleSelectionChange = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
     <>
-      <ObtainedCertificates />
-      <UnobtainedCertificates />
-      <AvailableCertificates />
+      <div className="menu">
+        <button onClick={() => handleSelectionChange("obtained")}>
+          Obtained Certificates
+        </button>
+        <button onClick={() => handleSelectionChange("unobtained")}>
+          Unobtained Certificates
+        </button>
+        <button onClick={() => handleSelectionChange("available")}>
+          Available Certificates
+        </button>
+      </div>
+
+      {selectedOption === "obtained" && <ObtainedCertificates />}
+      {selectedOption === "unobtained" && <UnobtainedCertificates />}
+      {selectedOption === "available" && <AvailableCertificates />}
     </>
   );
 };
