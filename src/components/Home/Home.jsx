4044214fe5cd1c6a3c8558da930cd728a7dc4f96
KonsBrou <mproumas.kostas@gmail.com>
2024-01-15 13:13:12 +0200

    appearence changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,41 +4,45 @@ import { Link } from "react-router-dom";
 import "./Home.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { Button, CardHeader, CardMedia, Typography, createTheme } from "@mui/material";
+import { Button, CardHeader, CardMedia, CssBaseline, Typography, createTheme } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 
 
 export default function Home({certificates, cookie}) {
 
   const theme = createTheme({
-    palette: {
-      background: {
-        paper: '#d1cfcf', // your color
+      palette: {
+        background: {
+          paper: '#ffffff', // your color
+        },
       },
-    },
-  });
+    }
+  );
   
   return (
+    <div className="backgroundGrad">
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       {cookie && cookie.candidateNumber !==0 && (<h1>Welcome, {cookie.firstName}!</h1>)}
         <>
-          <h2>Best Sellers</h2>
+          <h1>Best Sellers</h1>
           <div className="cards"style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
             {certificates?.slice(0, 3).map(({ $id: id, title, price, description, imageSrc }) => (
-              <Card key={id} sx={{ maxWidth: 345, flex: '1 0 30%', margin: '0.5rem'  }}>
+              <Card key={id} className="card" sx={{ maxWidth: 330, flex: '1 0 30%', margin: '0.5rem 2rem' }}>
                 <Link to={`/Certificate/${title}`} style={{textDecoration : "none"}}>
-                <CardHeader 
-                  title={
-                    <Typography variant="h6" noWrap sx={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap' }}>
-                      {title}
-                    </Typography>
-                  }
-                />
+                
                 <CardMedia
                   component="img"
                   height="194"
                   image={imageSrc}
                   sx={{objectFit: 'contain' }}
+                />
+                <CardHeader 
+                  title={
+                    <Typography variant="h6" noWrap sx={{ textOverflow: 'ellipsis', overflow: 'hidden', whiteSpace: 'nowrap',textAlign: 'center' }}>
+                      {title}
+                    </Typography>
+                  }
                 />
                 <CardContent>
                 <Typography variant="body2" color="text.secondary">
@@ -48,16 +52,19 @@ export default function Home({certificates, cookie}) {
                 </Link>
               </Card>
             ))}
-            {certificates && certificates.length > 3 && (
-              <Button variant="contained" size="large" sx={{ mt: 3, mb: 2 }} ><Link to="/Certificates" style={{textDecoration : "none"}}>View All</Link></Button>
-            )}
+            
           </div>
-
+          {certificates && certificates.length > 3 && (
+            <Button variant="contained" size="large" sx={{  m: "10px auto", display: "block" }}>
+              <Link to="/Certificates" style={{ textDecoration: "none", width: '100%', display: 'block' }}>
+                View All Certificates
+              </Link>
+            </Button>            )}
         </>
         <h2>Awesome Discounts</h2>
         <div className="cards"style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', margin: 'auto' }}>
           {certificates?.slice(3, 6).map(({ $id: id, title, price, description, imageSrc }) => (
-                <Card key={id} sx={{ maxWidth: 250, maxHeight:350, flex: '1 0 30%', margin: '0.5rem'  }}>
+                <Card key={id} className="card" sx={{ maxWidth: 250, maxHeight:350, flex: '1 0 30%', margin: '0.5rem' }}>
                   <Link to={`/Certificate/${title}`} style={{textDecoration : "none"}}>
                   <CardHeader 
                     title={
@@ -82,5 +89,6 @@ export default function Home({certificates, cookie}) {
               ))}
         </div>
     </ThemeProvider>
+    </div>
   );
 }
