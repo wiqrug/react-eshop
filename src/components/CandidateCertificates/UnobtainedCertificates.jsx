82295b65fb251e4fb4f06db14ae18b451acc4e89
KonsBrou <mproumas.kostas@gmail.com>
2024-01-18 01:23:59 +0200

    as

--- src/components/CandidateCertificates/UnobtainedCertificates.jsx
+++ src/components/CandidateCertificates/UnobtainedCertificates.jsx
@@ -5,7 +5,16 @@ import { useNavigate } from "react-router-dom";
 
 const UnobtainedCertificates = () => {
   const unobtainedCertificates = useCandidateCertificates("unobtained");
+  const navigate = useNavigate();
+
+  const handleTestClick = (title) => {
+    navigate(`/Exam/${title}`);
   }
+
+  const handleClick = (title) => {
+    navigate(`/Certificate/${title}`);
+  }
+
 
   return (
     <section>
@@ -27,24 +36,6 @@ const UnobtainedCertificates = () => {
           
         ))}
     </section>
-    <>
-      <section>
-        {unobtainedCertificates
-          // @ts-ignore
-          ?.map(({ title, description }) => (
-            <article key={title}>
-              <img
-                src="https://raw.githubusercontent.com/wiqrug/wiqrug.github.io/main/images/DALL%C2%B7E%202023-10-26%2018.43.43%20-%20Wide%20cartoon%20artwork%20with%20a%20gentle%20cream-colored%20backdrop.%20Playful%20anime%20clouds%20float%20around%2C%20some%20with%20cute%20expressions%2C%20ensuring%20the%20middle%20remains%20.png"
-                alt="img-logo"
-              />
-
-              <h4>{title}</h4>
-              <h4>{description}</h4>
-              <p>Click here to take the exam test</p>
-            </article>
-          ))}
-      </section>
-    </>
   );
 };
 
