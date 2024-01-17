d8f25adc87066f067ed3915d724096c084973480
jsy10 <74418510+jsy10@users.noreply.github.com>
2024-01-17 15:42:13 +0200

    oti na nai push

--- src/api/exams/updateExam.js
+++ src/api/exams/updateExam.js
@@ -1,4 +1,11 @@
 import { instance as axios } from "api/axios";
 
-export const updateExam = async(title) => {
+export const updateExam = async(Title, payload) => {
+  try {
+    const response = await axios.put(`Exams/${Title}`, payload);
+    return response.data; // Assuming the response data contains the candidate details
+  } catch (error) {
+    console.error("Error updating exam ", error);
+    throw error; // Re-throw the error to handle it in the calling function
+  }
 };
---
+++ src/components/Modals/AddExamModal.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button, Dialog, DialogActions, DialogContent } from "@mui/material";
+import ExamFormField from "components/FormFields/ExamFormField";
+import useFormFields from "hooks/useFormFields";
+
+const AddExamModal = ({ open, onClose, onSave }) => {
+  const { fields, handleFieldChange } = useFormFields({
+    Title: "",
+    title: "",
+    description: "",
+    time: ""
+    // Add all other fields here
+  });
+
+  const handleSave = () => {
+    console.log("From Add Exam Modal", fields);
+    onSave(fields);
+    onClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogContent>
+        <ExamFormField {...fields} onChange={handleFieldChange} />
+      </DialogContent>
+      <DialogActions style={{ justifyContent: "space-between" }}>
+        <Button onClick={onClose}>Close</Button>
+        <Button onClick={handleSave}>Save Changes</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default AddExamModal;
